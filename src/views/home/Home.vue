26b28bfef6fdd18534bf91078658d06595e9af72
cherishPhoebe <cherish1224@outlook.com>
2021-06-06 22:43:07 +0800

    add main tab bar

---
+++ src/views/home/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <div>购物街</div>
+      </template>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  components: {
+    NavBar
+  }
+}
+
+NavBar
+</script>
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+</style>
